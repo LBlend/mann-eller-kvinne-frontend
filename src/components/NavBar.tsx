369172738defc86c826f50df5eef3cd555f991cd
LBlend <lblend@protonmail.com>
2021-12-12 15:37:20 +0100

    navbar styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,9 @@
+import styles from "./NavBar.module.css";
+
 import { Link } from "react-router-dom";
 
 const NavBar = () => (
-  <nav>
+  <nav id={styles.navbar}>
     <Link to="/about">Om siden</Link>
   </nav>
 );
