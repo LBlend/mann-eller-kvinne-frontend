03c59446ef1e62fdd11928c603bf73e97c3b70d7
LBlend <lblend@protonmail.com>
2021-12-12 15:16:38 +0100

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
-function Footer() {
-  return (
-    <footer>
-      <p>
-        Laget av <a href="https://lblend.moe">LBlend</a> og{" "}
-        <a href="https://github.com/marksverdhei">marksverdhei</a>
-      </p>
-      <a href="https://github.com/LBlend/mann-eller-kvinne">Kildekode</a>
-    </footer>
-  );
-}
+import styles from "./Footer.module.css";
+
+const Footer = () => (
+  <footer id={styles.footer}>
+    <p>
+      Laget av <a href="https://lblend.moe">LBlend</a> og{" "}
+      <a href="https://github.com/marksverdhei">marksverdhei</a>
+    </p>
+    <a href="https://github.com/LBlend/mann-eller-kvinne">Kildekode</a>
+  </footer>
+);
 
 export default Footer;
