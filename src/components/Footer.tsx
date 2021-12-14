fada3c9d0e75c7bce7bffafc9aaff322236e26da
LBlend <lblend@protonmail.com>
2021-12-14 15:42:10 +0100

    change LBlend footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css";
 const Footer = () => (
   <footer id={styles.footer}>
     <p>
-      Laget av <a href="https://lblend.moe">LBlend</a> og{" "}
+      Laget av <a href="https://github.com/LBlend">LBlend</a> og{" "}
       <a href="https://github.com/marksverdhei">marksverdhei</a>
     </p>
     <a href="https://github.com/LBlend/mann-eller-kvinne">Kildekode</a>
