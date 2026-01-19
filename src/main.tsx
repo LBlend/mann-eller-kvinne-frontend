baaa0b3fc5bbdae11f30413f23c4dc2fee057b65
Leander Furumo <leander@furumo.eu>
2026-01-19 03:05:39 +0100

    migrate to react 18 and fix vulns

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { render } from "react-dom"
+import { createRoot } from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 // CSS files
@@ -13,13 +13,14 @@ import About from "./pages/About"
 import Footer from "./components/Footer"
 
 const rootElement = document.getElementById("root")
-render(
+if (!rootElement) throw new Error("Failed to find the root element")
+const root = createRoot(rootElement)
+root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
     </Routes>
     <Footer />
-  </BrowserRouter>,
-  rootElement
+  </BrowserRouter>
 )
